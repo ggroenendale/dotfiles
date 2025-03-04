367e9970fe276930b17f10f17b1632043e4c938c
Geoff Groenendale <g.groenendale@gmail.com>
2025-03-03 20:16:11 -0800

    Added lazy lua plugin loader file

---
+++ .config/nvim/lua/geoff/lazy.lua
@@ -0,0 +1,23 @@
+-- ~/.config/nvim/init.lua
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ { import = "geoff.plugins"} , { import = "geoff.plugins.lsp"} } , {
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+})
