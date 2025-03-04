7eb11885f7194d7cd3021756edeec8d63dc78f05
Geoff Groenendale <g.groenendale@gmail.com>
2025-03-03 20:15:31 -0800

    Added Neovim init file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+require("geoff.core")
+require("geoff.lazy")
+
+local function find_venv()
+	local cwd = vim.fn.getcwd() -- Get the current working directory
+	local venv_path = cwd .. "/.venv/bin/python"
+
+	if vim.fn.filereadable(venv_path) == 1 then
+		vim.g.python3_host_prog = venv_path
+	end
+end
+
+find_venv()
