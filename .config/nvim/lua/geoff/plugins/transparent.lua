9b5dc307b40137ee1fc3818a18e2edbaa6380979
Geoff Groenendale <g.groenendale@gmail.com>
2025-07-07 13:18:30 -0700

    Added transparent vim commands here.

--- .config/nvim/lua/geoff/plugins/transparent.lua
+++ .config/nvim/lua/geoff/plugins/transparent.lua
@@ -1,14 +1,48 @@
 return {
 	"xiyaowong/transparent.nvim",
+	lazy = false,
 	config = function()
 		require("transparent").setup({
 			enable = true,
-			extra_groups = {
-				"NormalFloat",
-				"NvimTreeNormal",
-				"TelescopeNormal",
+			groups = {
+				"Normal",
+				"NormalNC",
+				"Comment",
+				"Constant",
+				"Special",
+				"Identifier",
+				"Statement",
+				"PreProc",
+				"Type",
+				"Underlined",
+				"Todo",
+				"String",
+				"Function",
+				"Conditional",
+				"Repeat",
+				"Operator",
+				"Structure",
+				"LineNr",
+				"NonText",
+				"SignColumn",
+				"CursorLine",
+				"CursorLineNr",
+				"StatusLine",
+				"StatusLineNC",
+				"EndOfBuffer",
 			},
 			exclude_groups = {},
 		})
+
+		vim.cmd([[
+            highlight Normal guibg=none
+            highlight NonText guibg=none
+            highlight Normal ctermbg=none
+            highlight NonText ctermbg=none
+            highlight Special guibg=none
+            highlight Special ctermbg=none
+            highlight NormalNC guibg=none
+            highlight NormalNC ctermbg=none
+        ]])
 	end,
 }
