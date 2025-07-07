88cc2d0cc6bcf87a20cc6453d645bc57119b6769
Geoff Groenendale <g.groenendale@gmail.com>
2025-07-07 13:21:35 -0700

    Replacing deprecated diagnostics function

--- .config/nvim/lua/geoff/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/geoff/plugins/lsp/lspconfig.lua
@@ -85,7 +85,8 @@ return {
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			vim.diagnostic.config()
+			--vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
 		-- Customize the documentation hover window
