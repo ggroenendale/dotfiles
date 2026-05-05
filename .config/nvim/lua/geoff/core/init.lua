ced2aef494057241a97b24ada0ee41d0ffaf81b9
Geoff Groenendale <g.groenendale@gmail.com>
2026-05-05 09:36:56 -0700

    Fully deleted all items in .config/ folder and moved to stow directories

--- .config/nvim/lua/geoff/core/init.lua
+++
@@ -1,7 +0,0 @@
-require("geoff.core.options")
-require("geoff.core.keymaps")
-
--- Load GUI configuration if running in GUI mode
-if vim.g.neovide or vim.fn.has("gui_running") == 1 then
-    require("geoff.core.gui").setup()
-end
