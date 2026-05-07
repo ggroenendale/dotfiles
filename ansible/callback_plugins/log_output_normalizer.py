5fa0f7cbc652c690b9f02eadb62747deb8d11afe
Geoff Groenendale <g.groenendale@gmail.com>
2026-05-07 12:36:34 -0700

    Testing custom colors in log

--- ansible/callback_plugins/log_output_normalizer.py
+++ ansible/callback_plugins/log_output_normalizer.py
@@ -21,19 +21,22 @@ class CallbackModule(CallbackBase):
     YELLOW = "\033[33m"
     RESET = "\033[0m"
 
+    def v2_playbook_on_start(self, playbook):
+        self._display.display(f"Starting Playbook....", color="blue")
+
     def v2_runner_on_ok(self, result):
         host = result._host.get_name()
         msg = result._result.get("msg", "")
 
         if msg:
-            self._display.display(f"{self.GREEN}{msg}{self.RESET}")
+            self._display.display(f"msg", color="green")
         else:
-            self._display.display(f"{self.GREEN}{host}: OK{self.RESET}")
+            self._display.display(f"{host}: OK", color="yellow")
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
         host = result._host.get_name()
-        self._display.display(f"{self.RED}{host}: FAILED{self.RESET}")
+        self._display.display(f"{host}: FAILED", color="red")
 
     def v2_runner_on_skipped(self, result):
         host = result._host.get_name()
-        self._display.display(f"{self.YELLOW}{host}: SKIPPED{self.RESET}")
+        self._display.display(f"{host}: SKIPPED", color="yellow")
