977cf5cb74e470b4d365b2ff3cfac2b3ab7afebb
Geoff Groenendale <g.groenendale@gmail.com>
2026-05-07 16:33:17 -0700

    More color codes

--- ansible/callback_plugins/log_output_normalizer.py
+++ ansible/callback_plugins/log_output_normalizer.py
@@ -102,9 +102,9 @@ class CallbackModule(CallbackBase):
         os.makedirs(os.path.dirname(self.log_file), exist_ok=True)
 
         if color is None:
-            color = "\e[0;32m"
+            color = "\\x1b[38;5;9m"
 
-        reset = "\e[0m"
+        reset = "\x1b[37m"
 
         with open(log_file, "a") as f:
             f.write(f"{color}{data} {reset}\n")
