13626b2cd5192766a975c64db29666bd25a9b3a6
Geoff Groenendale <g.groenendale@gmail.com>
2026-05-08 12:48:02 -0700

    nested props

--- ansible/runner.py
+++ ansible/runner.py
@@ -83,7 +83,8 @@ variable_manager = VariableManager(loader=loader, inventory=inventory)
 
 extra_vars = load_extra_vars(loader=loader)
 variable_manager._extra_vars = extra_vars
-variable_manager._options_vars["ansible_version"] = ansible_version
+variable_manager._options_vars["ansible_version"]["string"] = ansible_version
+variable_manager._options_vars["ansible_version"]["full"] = ansible_version
 
 pprint(variable_manager.__dict__)
 
