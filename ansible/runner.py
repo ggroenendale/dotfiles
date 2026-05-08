b9e60b2cd0b58b25751ad9c2763b15cceca205bf
Geoff Groenendale <g.groenendale@gmail.com>
2026-05-07 20:24:14 -0700

    Created a new runner script to bypass ansible pull log statements

---
+++ ansible/runner.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from ansible.executor.playbook_executor import PlaybookExecutor
+from ansible.inventory.manager import InventoryManager
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.utils.display import Display
+import argparse
+
+# Custom arguments to get custom playbook url
+parser = argparse.ArgumentParser(
+    prog="Ansible Pull Runner",
+    description="Runs ansible pull with python API instead of CLI",
+    epilog="I dont know what this section is for",
+)
+
+# Define an argument for the filename
+parser.add_argument("filename")
+
+# Retrieve the args
+args = parser.parse_args()
+
+# Retrieve the playbook filename
+filename = args.filename
+
+loader = DataLoader()
+
+inventory = InventoryManager(loader=loader, sources=["localhost,"])
+variable_manager = VariableManager(loader=loader, inventory=inventory)
+
+passwords = {}
+
+playbook_path = Path(__file__).joinpath("playbooks", filename)
+
+# Instantiate a playbook executor to get rid of excessive ansible print statements
+executor = PlaybookExecutor(
+    playbooks=[playbook_path],
+    inventory=inventory,
+    variable_manager=variable_manager,
+    loader=loader,
+    passwords=passwords,
+)
+
+# Run the executor
+executor.run()
