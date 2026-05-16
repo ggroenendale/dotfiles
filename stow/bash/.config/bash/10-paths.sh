f77bb9e8c7becf5a66bbdae3a0c734dd317a9479
Geoff Groenendale <g.groenendale@gmail.com>
2026-05-15 23:50:09 -0700

    Added python path code from legacy ansible python config

--- stow/bash/.config/bash/10-paths.sh
+++ stow/bash/.config/bash/10-paths.sh
@@ -14,3 +14,13 @@ export PATH="$HOME/.local/bin:$PATH"
 # Pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+
+# Might need these other configurations?
+#echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
+
+# Legacy bash profile configs
+#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+#echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+#echo 'eval "$(pyenv init - bash)"' >> ~/.bash_profile
+
+
